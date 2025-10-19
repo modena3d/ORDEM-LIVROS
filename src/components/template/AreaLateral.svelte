a30257c6b37c83ae8ff0d7fe363176874da742d4
EmpresaFict <modena3d@gmail.com>
2025-10-19 17:40:23 -0300

    menu lateral

--- src/components/template/AreaLateral.svelte
+++ src/components/template/AreaLateral.svelte
@@ -1,23 +1,28 @@
 <script>
   import Menu from "./Menu.svelte";
+  import MenuItem from "./MenuItem.svelte";
 
 </script>
 <aside class="flex flex-col self-start
 border border-zinc-800 p-5 rounded-md gap-10 w-72">
  <Menu titulo="Navegação"> 
-  <li>Teste #1</li>
-  <li>Teste #2</li>
-  <li>Teste #3</li>
+   <MenuItem texto = "Inicio" href="/"> </MenuItem >
+   <MenuItem texto = "Personagens" href="/"> </MenuItem >
+   <MenuItem texto = "Autores" href="/autores"> </MenuItem >
+   <MenuItem texto = "Séries" href="/"> </MenuItem >
  </Menu>
-  <Menu titulo="principais Autores"> 
-  <li>Teste #1</li>
-  <li>Teste #2</li>
-  <li>Teste #3</li>
+  <Menu titulo="Principais Autores"> 
+   <MenuItem texto = "J.K. Rowling" href="/"> </MenuItem >
+   <MenuItem texto = "J.R.R. Tolkien" href="/"> </MenuItem >
+   <MenuItem texto = "George R.R. Martin" href="/autores/george-martin"> </MenuItem >
+   <MenuItem texto = "Séries" href="/"> </MenuItem >
  </Menu> 
  <Menu titulo="Outras Categorias"> 
-  <li>Teste #1</li>
-  <li>Teste #2</li>
-  <li>Teste #3</li>
+   <MenuItem texto = "Ultimas Notícias" href="/"> </MenuItem >
+   <MenuItem texto = "Séries Populares" href="/"> </MenuItem >
+   <MenuItem texto = "Facebook" href="/autores"> </MenuItem >
+   <MenuItem texto = "Intagram" href="/"> </MenuItem >
+   <MenuItem texto = "Youtube" href="/"> </MenuItem >
  </Menu>
 
 </aside>
